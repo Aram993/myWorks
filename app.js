1984361497715fb45c7420fe80db69971dc90b13
Aram993 <aram993@mail.ru>
2025-02-09 19:25:19 +0300

    create function

--- app.js
+++ app.js
@@ -2,26 +2,28 @@ const container = document.querySelector(".container");
 const loader = document.querySelector(".load");
 const UsersURL = fetch("https://dummyjson.com/users");
 
-UsersURL
-    .then(response => {
-        if (!response.ok) {
-            throw new Error("Ошибка запроса!");
-        }
-        return response.json();
-    })
-    .then(users => {
-        const user = users.users;
-        user.forEach(item => {
-            renderUsers(container, item.firstName, item.lastName, item.age, item.address.city, item.address.state, item.image);
+function getUsers (URL) {
+    URL
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Ошибка запроса!");
+            }
+            return response.json();
         })
-    })
-    .catch(err => {
-        console.error(err)
-    })
-    .finally(()=> {
-        loader.style.display = "none";
-        container.style.display = "grid";
-    })
+        .then(users => {
+            const user = users.users;
+            user.forEach(item => {
+                renderUsers(container, item.firstName, item.lastName, item.age, item.address.city, item.address.state, item.image);
+            })
+        })
+        .catch(err => {
+            console.error(err)
+        })
+        .finally(()=> {
+            loader.style.display = "none";
+            container.style.display = "grid";
+        })
+}
 
 function renderUsers(element, name, lastName, year, city, state, image) {
     element.innerHTML += `<div class="inner">
@@ -33,3 +35,5 @@ function renderUsers(element, name, lastName, year, city, state, image) {
                             <div class="info">${state}</div>
                         </div>`
 }
+
+getUsers(UsersURL);

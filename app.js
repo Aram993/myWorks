8b5598ba5e23b52255a4cae5ff56a14dc7eaca10
Aram993 <aram993@mail.ru>
2025-02-14 13:51:27 +0300

    second commit

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ const booksURL = "http://localhost:3000/books";
 
 const container = document.querySelector(".container");
 
-
-async function getData (URL) {
+const authorsArray = [];
+async function getData (URL, array) {
     try {
         const response = await axios.get(URL);
+        array.push(...response.data);
         return response.data;
     } catch (error) {
         console.error(error);
     };
 }
 
-getData(authorsURL);
-const authorsArray = getData(authorsURL);
-console.log(authorsArray);
+getData(authorsURL, authorsArray);
+
+function renderAuthors (element, array) {
+    array.forEach(item => {
+        
+    })
+}

736c38ad67e6e7c7b9e1c4fab52d017913bde17a
Aram993 <aram993@mail.ru>
2025-02-14 14:20:13 +0300

    third commit

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
 const authorsURL = "http://localhost:3000/authors";
 const booksURL = "http://localhost:3000/books";
 
+const select = document.querySelector("#authors");
 const container = document.querySelector(".container");
 
 const authorsArray = [];
-async function getData (URL, array) {
+async function getData (URL) {
     try {
         const response = await axios.get(URL);
-        array.push(...response.data);
+        authorsArray.push(response.data);
         return response.data;
     } catch (error) {
         console.error(error);
     };
 }
 
-getData(authorsURL, authorsArray);
+getData(authorsURL);
 
-function renderAuthors (element, array) {
+const renderAuthors = (element, array) => {
     array.forEach(item => {
-        
-    })
-}
+       element.innerHTML += `<option value="${item.id}">${item.name}</option>`
+    });
+};
+
+console.log(authorsArray.length);
+
+select.innerHTML += `<option value="Max">Max</option>`

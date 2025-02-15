c9344aa67cfe7c1cf521988269d699234ac1d684
Aram993 <aram993@mail.ru>
2025-02-15 22:41:19 +0300

    fourth commit

--- app.js
+++ app.js
@@ -3,26 +3,96 @@ const booksURL = "http://localhost:3000/books";
 
 const select = document.querySelector("#authors");
 const container = document.querySelector(".container");
+const pages = document.querySelectorAll(".page");
+const cardsField = document.querySelector(".cards-field");
+const skip = document.querySelectorAll(".skip");
 
 const authorsArray = [];
-async function getData (URL) {
+const booksArray = [];
+async function getAuthors (URL) {
     try {
         const response = await axios.get(URL);
-        authorsArray.push(response.data);
-        return response.data;
+        authorsArray.push(...response.data);
+        renderAuthors(select, authorsArray);
     } catch (error) {
         console.error(error);
     };
 }
 
-getData(authorsURL);
+async function getAllBooks (URL) {
+    try {
+        const response = await axios.get(URL);
+        booksArray.push(...response.data);
+    } catch (error) {
+        console.error(error);
+    };
+};
+
+getAuthors(authorsURL);
+getAllBooks(booksURL);
+getBooks(1);
 
 const renderAuthors = (element, array) => {
     array.forEach(item => {
-       element.innerHTML += `<option value="${item.id}">${item.name}</option>`
+        element.insertAdjacentHTML("beforeEnd", `<option value="${item.id}">${item.name}</option>`);
     });
 };
 
-console.log(authorsArray.length);
+async function getBooks (id) {
+    try {
+        const response = await axios.get(`http://localhost:3000/books?_page=${id}&_limit=4`);
+        renderBooks(authorsArray, response.data);
+    } catch (error) {
+        console.error(error);
+    }
+}
 
-select.innerHTML += `<option value="Max">Max</option>`
+function renderBooks (arr, arr2) {
+    cardsField.textContent = "";
+    arr.forEach(item => {
+        arr2.forEach(value => {
+            if (item.id === value.authorId) {
+                cardsField.innerHTML += `   <div class="card">
+                                                <div>${value.title}</div>
+                                                <div>${item.name}</div>   
+                                            </div>`
+            }
+        })
+    })
+}
+
+pages.forEach(page => {
+    page.addEventListener("click", ()=> {
+        clearPageColor(pages);
+        page.style.backgroundColor = "black";
+        page.style.color = "white";
+        getBooks(Number(page.textContent));
+    });
+});
+
+function clearPageColor (arr) {
+    arr.forEach(item => {
+        item.style.backgroundColor = "white";
+        item.style.color = "black";
+    });
+};
+
+select.addEventListener("change", ()=> {
+    if (select.value === "0") {
+        getBooks(1);
+        skip.forEach(item => {
+            item.style.display = "flex";
+        });
+
+    } else {
+        const filteredBooks = booksArray.filter(book => {
+            return book.authorId === Number(select.value);
+        });
+
+        skip.forEach(item => {
+            item.style.display = "none";
+        });
+        
+        renderBooks(authorsArray, filteredBooks);
+    };
+});
